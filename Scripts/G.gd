fceb1b0f2167f6b74a6b855440cf02f0f7d920d2
Purpursarkans <goga.danilov10@gmail.com>
2024-03-04 23:59:14 +0400

    переключение уровней на подключеных клиентах ( на новых клиентах уровня нет :с)

--- Scripts/G.gd
+++ Scripts/G.gd
@@ -7,6 +7,10 @@ var pause : bool = false
 
 var playerid
 
+var ADRESS = "127.0.0.1"
+var PORT : int = 25565
+
+
 func _process(delta):
 	#if Input.is_action_just_pressed("escape"):
 		#get_tree().quit()
